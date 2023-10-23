392664f576f34eef4d46f4807e334bc4940dde05
Uladzimir Tryputska <uladzimir.tryputska@servicelayers.com>
2023-10-23 16:21:22 +0200

    added proxy support

--- server/auth/types/openid/openid_auth.ts
+++ server/auth/types/openid/openid_auth.ts
@@ -29,6 +29,7 @@ import {
 } from 'opensearch-dashboards/server';
 import HTTP from 'http';
 import HTTPS from 'https';
+import { ProxyAgent } from 'proxy-agent';
 import { PeerCertificate } from 'tls';
 import { Server, ServerStateCookieOptions } from '@hapi/hapi';
 import { SecurityPluginConfigType } from '../../..';
@@ -179,15 +180,21 @@ export class OpenIdAuthentication extends AuthenticationType {
     if (Object.keys(this.wreckHttpsOption).length > 0) {
       return wreck.defaults({
         agents: {
-          http: new HTTP.Agent(),
-          https: new HTTPS.Agent(this.wreckHttpsOption),
-          httpsAllowUnauthorized: new HTTPS.Agent({
+          http: new ProxyAgent(),
+          https: new ProxyAgent(wreckHttpsOption),
+          httpsAllowUnauthorized: new ProxyAgent({
             rejectUnauthorized: false,
           }),
         },
       });
     } else {
-      return wreck;
+      return wreck.defaults({
+        agents: {
+          http: new ProxyAgent(),
+          https: new ProxyAgent(),
+          httpsAllowUnauthorized:  new ProxyAgent()
+        }
+      });
     }
   }
 
